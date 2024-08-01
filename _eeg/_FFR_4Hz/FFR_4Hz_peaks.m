7eab716239f6f0d2d1e6c7e4fa5f771796e119fc
Jonatan Marcher-Rorsted <j.marcher@hotmail.com>
2024-08-01 15:51:57 +0200

    peak analysis

--- _eeg/_FFR_4Hz/FFR_4Hz_peaks.m
+++ _eeg/_FFR_4Hz/FFR_4Hz_peaks.m
@@ -72,6 +72,8 @@ MNH_idx = find(age>25 & age<50 & ~CP )
 ONH_idx = find(age>=50 & ~CP);
 nh_idx = find(~CP); % all normal hearing
 ages = [17 77];
+% colormap
+uheal_colormap;
 % channels
 chansoi  = setdiff(1:16,[5 11]); % all channels but T7 and T8
 
@@ -117,3 +119,132 @@ ylabel('\muV')
 set(gca,'xtick',[1:6])
 box off
 
+%%  plot p2 relative to p1
+close all
+figure('renderer','painters')
+p2p1=P2-P1;
+n1p1 = N1-P1;
+n2p1 = N2-P1;
+subplot 241
+box off
+plot(1:6,nanmean(p2p1,1),'linewidth',2)
+hold on
+plot(1:6,nanmean(n1p1,1),'linewidth',2)
+plot(1:6,nanmean(n2p1,1),'linewidth',2)
+xlabel('tone nr.')
+ylabel('\muV')
+set(gca,'xtick',[1:6])
+xlim([0 7])
+titles={'P200-P50','N100-P50','N200-P50'};
+hleg =legend(titles)
+hleg.Box = 'off'
+title('Relative peak amplitude')
+box off
+ylim([-5 2])
+p_all = {p2p1;n1p1;n2p1};
+%
+for ii=1:3
+subplot(2,4,ii+1)
+errorbar(1:6,nanmean(p_all{ii}(YNH_idx,:)),nanstd(p_all{ii}(YNH_idx,:))/sqrt(length(YNH_idx)),'color',y_col,'marker','o','markerfacecolor',y_col);%,'o-','color',y_col)
+hold on
+errorbar(1:6,nanmean(p_all{ii}(MNH_idx,:)),nanstd(p_all{ii}(MNH_idx,:))/sqrt(length(MNH_idx)),'color',y_col,'marker','sq','markerfacecolor',m_col);%,'o-','color',y_col)
+errorbar(1:6,nanmean(p_all{ii}(ONH_idx,:)),nanstd(p_all{ii}(ONH_idx,:))/sqrt(length(ONH_idx)),'color',y_col,'marker','^','markerfacecolor',o_col);%,'o-','color',y_col)
+%ylim([-2 0.5])
+box off
+xlabel('tone nr.')
+ylabel('\muV')
+set(gca,'xtick',[1:6])
+xlim([0 7])
+title(titles{ii})
+if ii==1
+hleg=legend({'Young','Mid. aged','Older'})
+end
+hleg.Box = 'off'
+end
+subplot(2,4,[5:8])
+idx_all = {YNH_idx,MNH_idx,ONH_idx};
+gcol = {y_col,m_col,o_col};
+for ii=1:3
+plot(time_TS,nanmean(TS_base(idx_all{ii},:)),'color',gcol{ii})
+hold on
+end
+box off
+xlim([-0.5 3.5])
+ylabel('\muV')
+xlabel('Time (s)')
+
+fs = 1024;
+tstim  = 0:1/fs:0.25
+ramp = hann(100);win=[ramp(1:end/2-1)' ones(length(tstim)-100,1)' ramp(end/2:end)'];
+stim = [sin(2*pi*256*tstim).*win zeros(size(tstim))];
+stim_all = [];
+for ii=1:6
+    stim_all = [stim_all stim]
+end
+stim_all = [stim_all zeros(size([tstim tstim]))];
+stime = 0:1/fs:length(stim_all)/fs-1/fs;
+plot(stime,(stim_all*0.2)-3,'color',[0.5 0.5 0.5 0.5])
+
+set(gcf,'position',[214 275 1059 429])
+
+fig = gcf;
+saveas(fig,'/work1/jonmarc/UHEAL_master/UHEAL_paper/_eeg/_FFR_4Hz/_outputs/figs/P50_rel_peaks','epsc')
+
+
+
+%% corr with age
+close all
+figure('renderer','painters')
+p_all = {p2p1;n1p1;n2p1};
+idx_all = {YNH_idx,MNH_idx,ONH_idx};
+gcol = {y_col,m_col,o_col};
+titles={'P200-P50','N100-P50','N200-P50'};
+for tt=1:6 % 6 tones
+    
+    for  ii=1:3
+        subplot(3,6,tt+(ii-1)*6)
+
+
+        scatter(age(nh_idx),p_all{ii}(nh_idx,tt),'markeredgecolor',gcol{2},'marker','.','sizeData',50)
+        ll=lsline
+        ll.LineWidth =2;
+        ll.Color = 'k';
+        hold on
+        if ii==1
+            tit=title(['Tone nr. ' num2str(tt)]);
+            tit.FontName = 'Arial'
+        end
+        if tt==1
+            ylab=ylabel(titles{ii});
+            ylab.Rotation = 0;
+            ylab.Position = ylab.Position-[25 0 0];
+            ylab.FontWeight = 'bold'
+            ylab.FontName = 'Arial'
+        end
+
+
+    end
+end
+set(gcf,'position',[303 163 829 540])
+fig = gcf;
+saveas(fig,'/work1/jonmarc/UHEAL_master/UHEAL_paper/_eeg/_FFR_4Hz/_outputs/figs/P50_rel_peaks_scatter','epsc')
+
+%% tone 2- tone 1 response
+close all
+figure('renderer','painters')
+for pp =1:3
+    subplot(1,3,pp)
+    scatter(age(nh_idx),p_all{pp}(nh_idx,2)-p_all{pp}(nh_idx,1),'markeredgecolor',gcol{2},'marker','.','sizeData',50)
+    ll=lsline
+    ll.LineWidth =2;
+    ll.Color = 'k';
+    title(titles{pp})
+    if pp==1
+        ylabel('Tone nr.2 - Tone nr. 1')
+    end
+    ylim([-6 6])
+    %set(gca,'fontsize',12)
+end
+set(gcf,'position',[440 552 494 151])
+fig = gcf;
+saveas(fig,'/work1/jonmarc/UHEAL_master/UHEAL_paper/_eeg/_FFR_4Hz/_outputs/figs/P50_rel_peaks_t2-t1','epsc')
